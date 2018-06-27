97400f0cd4746335cf40acdd2d2468865e5cf29f
Pablo Jara <pablo.jara@dataart.com>
2018-06-27 16:37:46 -0300

    change1

--- movie-app/src/nav/Nav.js
+++ movie-app/src/nav/Nav.js
@@ -12,12 +12,13 @@ class NavComp extends Component {
       <Router>
         <div>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="">MovieApp</a>
+            <a className="navbar-brand" href="#">MovieApp</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div className="navbar-nav">
+
                 <Link to="/" className="nav-item nav-link active" >Home <span className="sr-only">(current)</span></Link>
                 <Link to="/Peliculas" className="nav-item nav-link" href="">Películas</Link>
                 <Link to="/Series" className="nav-item nav-link" href="">Series</Link>
